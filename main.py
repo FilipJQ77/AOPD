0c3407bd8b0f8b3f7ab0717babbf4e650111f528
FilipJQ77 <filipprzygonski99@gmail.com>
2021-10-23 23:36:38 +0200

    Add abstract calculator class to generalise function calculating, add user input from console, quantiles are optional now

--- main.py
+++ main.py
@@ -1,33 +1,61 @@
-import math
-from math import sin, cos
+from abc import ABC, abstractmethod
+from math import sin, cos, inf
 from numpy import linspace
 import matplotlib.pyplot as plt
 import statistics
 
 
-def function(x):
-    return (sin(x) + 2 * cos(x) ** 2 + x ** 7 - x) / x ** 2
+class Calculator(ABC):
+    @abstractmethod
+    def calculate(self, x_points, math_function, number_of_samples):
+        pass
+
+
+class NaiveCalculator(Calculator):
+    def calculate(self, x_points, math_function, number_of_samples):
+        y_points = [0] * number_of_samples
+        for i in range(len(x_points)):
+            y_points[i] = math_function(x_points[i])
+        return y_points
+
+
+class AcceleratedCalculator(Calculator):
+    def calculate(self, x_points, math_function, number_of_samples):
+        y_points = [0] * number_of_samples
+        # todo CUDA magic
+        return y_points
 
 
 def main():
-    # todo number of samples and quantiles changeable as a slider
-    number_of_samples = 100000
-    quantiles_number = 100
-    number_from = -5
-    number_to = 5
-    x_array = linspace(number_from, number_to, number_of_samples)
-    y_array = [0] * number_of_samples
-    for i in range(len(x_array)):
-        y_array[i] = function(x_array[i])
-    quantiles = statistics.quantiles(y_array, n=quantiles_number)
-    for i in range(len(y_array)):
-        if y_array[i] < quantiles[0]:
-            y_array[i] = -math.inf
-        if y_array[i] > quantiles[len(quantiles)-1]:
-            y_array[i] = math.inf
+    while True:
+        calculation_type = int(input("1. Naive\n2. Accelerated\n"))
+        if calculation_type == 1:
+            calculator = NaiveCalculator()
+        elif calculation_type == 2:
+            calculator = AcceleratedCalculator()
+        else:
+            return
 
-    plt.plot(x_array, y_array)
-    plt.waitforbuttonpress()
+        def math_function(x: float) -> float:
+            return sin(x) + 2 * cos(x) ** 2 - x
+
+        number_from = float(input("Number from: "))
+        number_to = float(input("Number to: "))
+        number_of_samples = int(input("Number of samples: "))
+        quantile_number = int(input("Quantile number (0 if no quantiles): "))
+
+        x_array = linspace(number_from, number_to, number_of_samples)
+        y_array = calculator.calculate(x_array, math_function, number_of_samples)
+        if quantile_number >= 1:
+            quantiles = statistics.quantiles(y_array, n=quantile_number)
+            for i in range(len(y_array)):
+                if y_array[i] < quantiles[0]:
+                    y_array[i] = inf
+                if y_array[i] > quantiles[len(quantiles) - 1]:
+                    y_array[i] = inf
+
+        plt.plot(x_array, y_array)
+        plt.waitforbuttonpress()
 
 
 if __name__ == '__main__':
