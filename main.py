f7a775ff3ca54bb0f5f452be393dd29b72740e3c
FilipJQ77 <filipprzygonski99@gmail.com>
2021-11-04 10:56:05 +0100

    Cleanup cleanup

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from math import sin, cos, inf
 from time import perf_counter_ns as timer
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pycuda.cumath as cumath
 import pycuda.gpuarray as gpuarray
-from numpy import linspace
 
 # import pycuda.driver as cuda
 # import pycuda.autoinit
@@ -15,8 +15,12 @@ from numpy import linspace
 function = "sin(x) + 2 * cos(x) ** 2 - x"  # variable with function for .csv file
 
 
-def math_function(x: float) -> float:
-    return sin(x) + 2 * cos(x) ** 2 - x
+def math_function_naive(x: float) -> float:
+    return sin(x) + 2 * cos(x) ** 2 - x  # todo is cumath possible here? if yes, then we can use one function for both
+
+
+def math_function_accelerated(x: gpuarray) -> gpuarray:
+    return cumath.sin(x) + 2 * cumath.cos(x) ** 2 - x
 
 
 class Calculator(ABC):
@@ -27,39 +31,23 @@ class Calculator(ABC):
 
 class NaiveCalculator(Calculator):
     def calculate(self, x_points, number_of_samples):
-        y_points = [0] * number_of_samples
+        y_points = np.zeros(number_of_samples, dtype=np.float64)
         for i in range(number_of_samples):
-            self.function = math_function(x_points[i])
-            y_points[i] = self.function
+            y_points[i] = math_function_naive(x_points[i])
         return y_points
 
 
 class AcceleratedCalculator(Calculator):
     def calculate(self, x_points, number_of_samples):
-        # y_points = [0] * number_of_samples
-        # function_gpu = cuda.mem_alloc_like(number_of_samples)
-        # cuda.memcpy_htod(function_gpu, number_of_samples)
-        # module = SourceModule(
-        #     """
-        #         __global__ void calculate(double *x_points, double *y_points, int number_of_samples)
-        #         {
-        #             // __device__ double sin ( double  x)
-        #
-        #             int idx = threadIdx.x + threadIdx.y*4;
-        #             y_point[idx] = sin(x) + 2 * cos(x) ** 2 - x;
-        #         }
-        #     """
-        # )
-        # result = module.get_function("calculate")
-        # result(function_gpu)
-
         # CUDA magic
+        y_points = np.zeros(number_of_samples, dtype=np.float64)
         x_array_gpu = gpuarray.to_gpu(x_points)
-        y_points = (cumath.sin(x_array_gpu) + 2 * cumath.cos(x_array_gpu) ** 2 - x_array_gpu).get()
+        math_function_accelerated(x_array_gpu).get(y_points)  # todo check if works, if not then use line below
+        # y_points = math_function_accelerated(x_array_gpu).get()
         return y_points
 
 
-#  ##  Additional  ##
+# optional: removing the highest and the lowest points if the plot is flat because of them
 def calculate_quantiles(y_array):
     quantile_number = int(input("Quantile number (0 if no quantiles): "))
     if quantile_number >= 1:
@@ -86,7 +74,7 @@ def main():
     number_of_samples = int(input("Number of samples: "))
 
     x_time_start = timer()
-    x_array = linspace(number_from, number_to, number_of_samples)
+    x_array = np.linspace(number_from, number_to, number_of_samples)
     x_time_stop = timer()
 
     y_time_start = timer()
@@ -101,21 +89,30 @@ def main():
     y_array_time = y_time_stop - y_time_start
     plot_time = plot_time_stop - plot_time_start
 
-    # plt.waitforbuttonpress()  # display plot
-    with open("results.csv", 'a') as file:
-        file.write(
-            f"{'Naive' if calculation_type == 1 else 'Accelerated'};\t"
-            f"{function};\t"
-            f"{number_from};\t"
-            f"{number_to};\t"
-            f"{number_of_samples};\t"
-            f"{x_array_time};\t"
-            f"{y_array_time};\t"
-            f"{plot_time};\t"
-            f"\n")
+    plt.waitforbuttonpress()  # display plot
 
-        # create header for new file
-        # file.write(f"math_function;number_from;number_to;number_of_samples;x_array_time;y_array_time;plot_time")
+    # create header for new file
+    with open("results.csv", 'a') as file:
+        # file.write(
+        #     "Implementation;"
+        #     "Math function;"
+        #     "Number from;"
+        #     "Number to;"
+        #     "Number of samples;"
+        #     "Time - Creating x array;"
+        #     "Time - Calculating y array;"
+        #     "Time - Plotting results"
+        #     "\n")
+        file.write(
+            f"{'Naive' if calculation_type == 1 else 'Accelerated'};"
+            f"{function};"
+            f"{number_from};"
+            f"{number_to};"
+            f"{number_of_samples};"
+            f"{x_array_time};"
+            f"{y_array_time};"
+            f"{plot_time}"
+            f"\n")
 
 
 if __name__ == '__main__':
