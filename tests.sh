ae04854a0d30614ad0b79cdcc5959eb7907dacbb
Maciej Byczko <byczqq136@gmail.com>
2021-11-07 13:45:21 +0100

    change numer of repetitions and test order
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

--- tests.sh
+++ tests.sh
@@ -3,7 +3,7 @@
 # <plik> <typ: 1 - naive, 2 - accelerated, <start_point>, <stop_point>, <samples>, <function 1-6>, <repetitions>, <show plot>
 samples=1000000
 function=1
-repetitions=1
+repetitions=10
 show_plot=0
 
 filename="diff_points.csv"
@@ -18,7 +18,7 @@ start_point=1
 stop_point=10
 filename="results.csv"
 for function in {1,2,3,4,5,6};do
-    for implementation in {2,1};do
+    for implementation in {1,2};do
         for samples in {1000,10000,100000,1000000,10000000,100000000};do
             echo "$implementation $start_point $stop_point $samples $function $repetitions $show_plot"
             python "./main.py" "$implementation" "$start_point" "$stop_point" "$samples" "$function" "$repetitions" "$show_plot" "$filename"
